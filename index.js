5a73911e8cbf6ecbf4b874c67dafd9c7ce94e71b
Diego Moreno <iam@diegomoreno.co>
2019-04-01 13:48:15 -0500

    Test example server-client

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
 var app = require('express')();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
+const io = require('socket.io')(http);
+const port = process.env.MODE ? 3000 : 80 || 80
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function (socket) {
-  console.log('a user connected');
+  console.info(`Client connected [id=${socket.id}]`);
+  // initialize this client's sequence number
+  sequenceNumberByClient.set(socket, 1);
 
-  socket.on('chat message', function (msg) {
-    console.log('message: ' + msg);
-    io.emit('chat message', msg);
-  });
-
-  socket.on('disconnect', function () {
-    console.log('user disconnected');
+  // when socket disconnects, remove it from the list:
+  socket.on("disconnect", () => {
+    sequenceNumberByClient.delete(socket);
+    console.info(`Client gone [id=${socket.id}]`);
   });
 });
 
-http.listen(3000, function () {
-  console.log('listening on *:3000');
+http.listen(port, function () {
+  console.log(`listening on *:${port}`);
 });
+
+// sends each client its current sequence number
+setInterval(() => {
+  for (const [client, sequenceNumber] of sequenceNumberByClient.entries()) {
+    client.emit("seq-num", sequenceNumber);
+    sequenceNumberByClient.set(client, sequenceNumber + 1);
+  }
+}, 1000);
