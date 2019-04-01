188fbecc401a232c9141cd47d1f631cd5babd8c1
Diego Moreno <iam@diegomoreno.co>
2019-04-01 13:50:26 -0500

    Client ip updated

--- client.js
+++ client.js
@@ -1,5 +1,5 @@
 const io = require("socket.io-client"),
-      ioClient = io.connect("http://localhost:8000");
+      ioClient = io.connect("134.209.70.254:80");
 
 ioClient.on("seq-num", (msg) => {
   console.info(msg);
